b56c420fc321600a7f1c3ec42a26153c8d1e708c
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-02-24 16:19:01 +0200

    Add Spotify Telegram bot login via MongoDB

---
+++ src/otsom.fs.Telegram.Bot.Auth.Spotify.Mongo/Mappings.fs
@@ -0,0 +1,34 @@
+namespace otsom.fs.Telegram.Bot.Auth.Spotify.Mongo
+
+open otsom.fs.Telegram.Bot.Auth.Spotify.Mongo
+open otsom.fs.Telegram.Bot.Auth.Spotify.Workflows
+open otsom.fs.Telegram.Bot.Core
+
+module internal Mappings =
+  [<RequireQualifiedAccess>]
+  module Inited =
+    let fromDb (auth: Entities.Auth) : Inited =
+      { State = State.parse auth.State
+        UserId = auth.UserId |> UserId }
+
+    let toDb (auth: Inited) : Entities.Auth =
+      Entities.Auth(UserId = (auth.UserId |> UserId.value), State = (auth.State |> State.value))
+
+  [<RequireQualifiedAccess>]
+  module Fulfilled =
+    let fromDb (auth: Entities.Auth) : Fulfilled =
+      { State = State.parse auth.State
+        UserId = auth.UserId |> UserId
+        Code = Code auth.Code }
+
+    let toDb (auth: Fulfilled) : Entities.Auth =
+      Entities.Auth(UserId = (auth.UserId |> UserId.value), State = (auth.State |> State.value), Code = (auth.Code |> Code.value))
+
+  [<RequireQualifiedAccess>]
+  module Completed =
+    let toDb (auth: Completed) : Entities.UserToken =
+      Entities.UserToken(UserId = (auth.UserId |> UserId.value), RefreshToken = auth.Token)
+
+    let fromDb (token: Entities.UserToken) : Completed =
+      { UserId = (token.UserId |> UserId)
+        Token = token.RefreshToken }
