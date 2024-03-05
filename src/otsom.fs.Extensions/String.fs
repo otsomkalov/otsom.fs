f8e584413754f3e30e1a110a9b7124dfffdf93d9
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-03-05 20:40:40 +0200

    Add string Contains active recognizer

--- src/otsom.fs.Extensions/String.fs
+++ src/otsom.fs.Extensions/String.fs
@@ -2,14 +2,20 @@
 
 open System
 
-let (|StartsWith|_|) (substring: string) (str: string) =
-  if str.StartsWith(substring, StringComparison.InvariantCultureIgnoreCase) then
+let (|StartsWith|_|) (substring: string) (source: string) =
+  if source.StartsWith(substring, StringComparison.InvariantCultureIgnoreCase) then
     Some()
   else
     None
 
 let (|Equals|_|) (toCompare: string) (source: string) =
   if String.Equals(source, toCompare, StringComparison.InvariantCultureIgnoreCase) then
+    Some()
+  else
+    None
+
+let (|Contains|_|) (substring: string) (source: string) =
+  if source.StartsWith(substring, StringComparison.InvariantCultureIgnoreCase) then
     Some()
   else
     None
