f59070761ba711f6aaebdc8ccef8357e1871e4e2
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-03-08 18:45:29 +0200

    Add Option.taskMap

---
+++ src/otsom.fs.Extensions/Option.fs
@@ -0,0 +1,10 @@
+﻿[<RequireQualifiedAccess>]
+module otsom.fs.Extensions.Option
+
+open System.Threading.Tasks
+
+let taskMap (mapping: 'a -> Task<'b>) option =
+  match option with
+  | Some v -> mapping v |> Task.map Some
+  | None -> None |> Task.FromResult
+
