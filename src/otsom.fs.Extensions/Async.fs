58a3465c63b6b18b9ffcbae94505128753f2d4f5
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-04-18 17:21:56 +0300

    Add basic operations for F# Async (#35)

---
+++ src/otsom.fs.Extensions/Async.fs
@@ -0,0 +1,16 @@
+﻿[<RequireQualifiedAccess>]
+module otsom.fs.Extensions.Async
+
+let inline map ([<InlineIfLambda>] mapper: 'a -> 'b) (task': Async<'a>) : Async<'b> =
+  async {
+    let! value = task'
+
+    return mapper value
+  }
+
+let inline bind ([<InlineIfLambda>] binder: 'a -> Async<'b>) (task': Async<'a>) : Async<'b> =
+  async {
+    let! value = task'
+
+    return! binder value
+  }
