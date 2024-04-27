9ad47a11f0d051875985b69e21177a2609c1e9b0
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-04-27 22:32:18 +0300

    Add TaskOption (#40)

---
+++ src/otsom.fs.Extensions/TaskOption.fs
@@ -0,0 +1,8 @@
+﻿[<RequireQualifiedAccess>]
+module otsom.fs.Extensions.TaskOption
+
+open System.Threading.Tasks
+
+let inline map ([<InlineIfLambda>] mapping: 'a -> 'b) = Task.map (Option.map mapping)
+
+let inline taskMap ([<InlineIfLambda>] mapping: 'a -> Task<'b>) = Task.bind (Option.taskMap mapping)
