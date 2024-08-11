ef4eed2080d81f67ac6ed06fc1d3ceee8f1c1397
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-08-11 16:03:35 +0300

    Add tap function for Option and TaskOption

--- src/otsom.fs.Extensions/Option.fs
+++ src/otsom.fs.Extensions/Option.fs
@@ -12,3 +12,10 @@ let defaultWithTask (defThunkTask: unit -> Task<'a>) option : Task<'a> =
   match option with
   | Some v -> v |> Task.FromResult
   | None -> defThunkTask ()
+
+let inline tap (effect: 'a -> unit) (option: 'a option) : 'a option =
+  match option with
+  | Some v ->
+    do effect v
+    option
+  | None -> option
--- src/otsom.fs.Extensions/TaskOption.fs
+++ src/otsom.fs.Extensions/TaskOption.fs
@@ -6,3 +6,6 @@ open System.Threading.Tasks
 let inline map ([<InlineIfLambda>] mapping: 'a -> 'b) = Task.map (Option.map mapping)
 
 let inline taskMap ([<InlineIfLambda>] mapping: 'a -> Task<'b>) = Task.bind (Option.taskMap mapping)
+
+let inline tap ([<InlineIfLambda>] effect: 'a -> unit) (taskOption: Task<'a option>) : Task<'a option> =
+  taskOption |> Task.map (Option.tap effect)
