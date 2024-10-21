766dd4bf0ca0114a6acb8177959a78703f1626b1
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-10-21 23:08:22 +0300

    Add operators (#80)

---
+++ src/otsom.fs.Extensions/Operators.fs
@@ -0,0 +1,11 @@
+﻿[<AutoOpen>]
+module otsom.fs.Extensions.Operators
+
+let (!>) = Task.map
+let (!!>) = Task.bind
+let (=>) = Result.map
+let (==>) = Result.bind
+let (!=>) = TaskResult.map
+
+let (!=!>) arg f =
+  arg |> (f >> Result.taskMap >> Task.bind)
