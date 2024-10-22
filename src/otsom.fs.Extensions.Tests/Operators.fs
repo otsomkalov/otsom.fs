cc5054f0f9d21a9a7ba444f54cc62d3ba7f0f8d8
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-10-22 14:19:38 +0300

    Add more operators

--- src/otsom.fs.Extensions.Tests/Operators.fs
+++ src/otsom.fs.Extensions.Tests/Operators.fs
@@ -8,7 +8,7 @@ open FsUnit.Xunit
 let private task' = 5 |> Task.FromResult
 
 [<Fact>]
-let ``(!|>) works`` () =
+let ``(&|>) works`` () =
   task {
     let! result = task' &|> (fun x -> x + 5)
 
@@ -16,14 +16,25 @@ let ``(!|>) works`` () =
   }
 
 [<Fact>]
-let ``(&|>&) works`` () =
+let ``(&|!) works`` () =
+  task {
+    let mutable result1 = 5
+
+    let! result2 = (task' &|! (fun x -> result1 <- result1 + 5))
+
+    result1 |> should equal 10
+    result2 |> should equal 5
+  }
+
+[<Fact>]
+let ``(&|&>) works`` () =
   task {
     let! result = task' &|&> (fun x -> Task.FromResult(x + 5))
 
     result |> should equal 10
   }
 
-let private result' : Result<int, unit> = Ok 5
+let private result': Result<int, unit> = Ok 5
 
 [<Fact>]
 let ``(=|>) works`` () =
@@ -51,9 +62,20 @@ let ``(&=|>) works`` () =
   }
 
 [<Fact>]
-let ``(&=|&>)``() =
+let ``(&=|&>) works`` () =
   task {
     let! result = taskResult &=|&> (fun x -> (x + 5) |> Task.FromResult)
 
     result |> should equal (Result<int, unit>.Ok 10)
+  }
+
+[<Fact>]
+let ``(&=|&!) works`` () =
+  task {
+    let mutable result1 = 5
+
+    let! result2 = taskResult &=|&! (fun x -> task { result1 <- result1 + 5 })
+
+    result1 |> should equal 10
+    result2 |> should equal (Result<int, unit>.Ok 5)
   }
--- src/otsom.fs.Extensions/Operators.fs
+++ src/otsom.fs.Extensions/Operators.fs
@@ -4,6 +4,9 @@ module otsom.fs.Extensions.Operators
 /// Task.map
 let (&|>) arg func = Task.map func arg
 
+/// Task.tap
+let (&|!) arg func = Task.tap func arg
+
 /// Task.bind
 let (&|&>) arg func = Task.bind func arg
 
@@ -18,3 +21,6 @@ let (&=|>) arg func = TaskResult.map func arg
 
 /// TaskResult.taskMap
 let (&=|&>) arg func = TaskResult.taskMap func arg
+
+/// TaskResult.taskTap
+let (&=|&!) arg func = TaskResult.taskTap func arg
