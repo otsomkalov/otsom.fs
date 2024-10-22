0c83a524527b1b824271219c7bcd53dd6db82d3e
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-10-22 13:45:01 +0300

    Fix & rename operators (#81)

---
+++ src/otsom.fs.Extensions.Tests/Operators.fs
@@ -0,0 +1,59 @@
+﻿module otsom.fs.Extensions.Tests.Operators
+
+open System.Threading.Tasks
+open Xunit
+open otsom.fs.Extensions.Operators
+open FsUnit.Xunit
+
+let private task' = 5 |> Task.FromResult
+
+[<Fact>]
+let ``(!|>) works`` () =
+  task {
+    let! result = task' &|> (fun x -> x + 5)
+
+    result |> should equal 10
+  }
+
+[<Fact>]
+let ``(&|>&) works`` () =
+  task {
+    let! result = task' &|&> (fun x -> Task.FromResult(x + 5))
+
+    result |> should equal 10
+  }
+
+let private result' : Result<int, unit> = Ok 5
+
+[<Fact>]
+let ``(=|>) works`` () =
+  let result = result' =|> (fun x -> x + 5)
+
+  result |> should equal (Result<int, unit>.Ok 10)
+
+
+[<Fact>]
+let ``(=|&>) works`` () =
+  task {
+    let! result = result' =|&> (fun x -> (x + 5) |> Task.FromResult)
+
+    result |> should equal (Result<int, unit>.Ok 10)
+  }
+
+let private taskResult = result' |> Task.FromResult
+
+[<Fact>]
+let ``(&=|>) works`` () =
+  task {
+    let! result = taskResult &=|> (fun x -> (x + 5))
+
+    result |> should equal (Result<int, unit>.Ok 10)
+  }
+
+[<Fact>]
+let ``(&=|&>)``() =
+  task {
+    let! result = taskResult &=|&> (fun x -> (x + 5) |> Task.FromResult)
+
+    result |> should equal (Result<int, unit>.Ok 10)
+  }
