d1cde2aaf8bb9e9d406f3ae25a1d60fa2d603312
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-10-25 20:06:02 +0300

    Add more operators (#82)

--- src/otsom.fs.Extensions/Operators.fs
+++ src/otsom.fs.Extensions/Operators.fs
@@ -4,11 +4,17 @@ module otsom.fs.Extensions.Operators
 /// Task.map
 let (&|>) arg func = Task.map func arg
 
+/// Task.tap
+let (&|!) arg func = Task.tap func arg
+
 /// Task.bind
 let (&|&>) arg func = Task.bind func arg
 
 /// Result.map
 let (=|>) arg func = Result.map func arg
+
+/// Result.bind
+let (=|=>) arg func = Result.bind func arg
 
 /// Result.taskMap
 let (=|&>) arg func = Result.taskMap func arg
@@ -18,3 +24,6 @@ let (&=|>) arg func = TaskResult.map func arg
 
 /// TaskResult.taskMap
 let (&=|&>) arg func = TaskResult.taskMap func arg
+
+/// TaskResult.taskTap
+let (&=|&!) arg func = TaskResult.taskTap func arg
