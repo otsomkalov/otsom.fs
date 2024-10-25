358061dc38b8862b437824aae0e7180ed6fbe72b
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-10-25 21:07:21 +0300

    Add shared Bot project

--- src/otsom.fs.Bot/Core.fs
+++ src/otsom.fs.Bot/Core.fs
@@ -1,8 +1,7 @@
-namespace otsom.fs.Bot.Core
+namespace otsom.fs.Bot
 
 open System.Threading.Tasks
 open Microsoft.FSharp.Core
-open otsom.fs.Core
 
 type BotMessageId = BotMessageId of int
 type ChatMessageId = ChatMessageId of int
@@ -18,133 +17,35 @@ module BotMessageId =
 type MessageButton = string * string
 type KeyboardButton = string
 
-type SendMessage = string -> Task<BotMessageId>
-type SendUserMessage = UserId -> SendMessage
-type ReplyToMessage = string -> Task<BotMessageId>
-type ReplyToUserMessage = UserId -> int -> ReplyToMessage
-// type SendMessageButtons = string -> InlineKeyboardMarkup -> Task<unit>
-// type SendUserMessageButtons = UserId -> SendMessageButtons
-// type SendKeyboard = string -> ReplyKeyboardMarkup -> Task<unit>
-// type SendUserKeyboard = UserId -> SendKeyboard
-// type ReplyWithKeyboard = string -> ReplyKeyboardMarkup -> Task<unit>
-// type ReplyUserWithKeyboard = UserId -> int -> ReplyWithKeyboard
-type AskForReply = string -> Task<unit>
-type AskUserForReply = UserId -> int -> AskForReply
-type EditMessage = string -> Task<unit>
-type EditBotMessage = UserId -> BotMessageId -> EditMessage
-// type EditMessageButtons = string -> InlineKeyboardMarkup -> Task<unit>
-// type EditBotMessageButtons = UserId -> BotMessageId -> EditMessageButtons
+type ChatId =
+  | ChatId of int64
 
-type ChatId = string
+  member this.Value =
+    let (ChatId id) = this
+    id
 
-type ConversationId = string
+type Keyboard = KeyboardButton seq seq
 
-type ConversationStepId = string
-
-type ConversationStep =
-  { Id: ConversationStepId
-    Resource: string
-    Next: ConversationStepId option }
-
-type ConversationTemplateId = string
-
-type ConversationTemplate =
-  { Id: ConversationTemplateId
-    FirstStep: ConversationStep
-    Steps: ConversationStep list }
-
-type CurrentStepData =
-  { BotMessageId: BotMessageId
-    ConversationStepId: ConversationStepId }
-
-type OngoingConversationStep =
-  { BotMessageId: BotMessageId
-    ChatMessageId: ChatMessageId
-    ConversationStepId: ConversationStepId
-    Data: string }
+type MessageButtons = MessageButton seq seq
 
 [<RequireQualifiedAccess>]
-module OngoingConversationStep =
-  let fromCurrentStepData (currentStepData: CurrentStepData) chatMessageId data : OngoingConversationStep =
-    { BotMessageId = currentStepData.BotMessageId
-      Data = data
-      ChatMessageId = chatMessageId
-      ConversationStepId = currentStepData.ConversationStepId }
+module Keyboard =
+  let empty = Seq.empty<string seq>
 
-type FinishedConversationStep =
-  { ConversationStepId: ConversationStepId
-    Data: string }
+type ISendMessage =
+  abstract member SendMessage: chatId: ChatId * text: string -> Task<BotMessageId>
 
-[<RequireQualifiedAccess>]
-module FinishedConversationStep =
-  let fromCurrentStepData (currentStepData: CurrentStepData) data : FinishedConversationStep =
-    { ConversationStepId = currentStepData.ConversationStepId
-      Data = data }
+type IEditMessage =
+  abstract member EditMessage: chatId: ChatId * messageId: BotMessageId * text: string -> Task
 
-  let fromOngoingStep (ongoing: OngoingConversationStep) =
-    { ConversationStepId = ongoing.ConversationStepId
-      Data = ongoing.Data }
+type IReplyToMessage =
+  abstract member ReplyToMessage: chatId: ChatId * messageId: ChatMessageId * text: string -> Task<BotMessageId>
 
-[<RequireQualifiedAccess>]
-module Conversation =
-  type New =
-    { Id: ConversationId
-      TemplateId: ConversationTemplateId
-      CurrentStepData: CurrentStepData }
+type ISendMessageButtons =
+  abstract member SendMessageButtons: chatId: ChatId * text: string * buttons: MessageButtons -> Task<BotMessageId>
 
-  type Ongoing =
-    { Id: ConversationId
-      TemplateId: ConversationTemplateId
-      CurrentStepData: CurrentStepData
-      StepsData: OngoingConversationStep list }
+type ISendKeyboard =
+  abstract member SendKeyboard: chatId: ChatId * text: string * buttons: Keyboard -> Task<BotMessageId>
 
-  [<RequireQualifiedAccess>]
-  module Ongoing =
-    let fromNew (new': New) chatMessageId data stepData : Ongoing =
-      { Id = new'.Id
-        TemplateId = new'.TemplateId
-        CurrentStepData = stepData
-        StepsData = [ OngoingConversationStep.fromCurrentStepData new'.CurrentStepData chatMessageId data ] }
-
-  let create =
-    fun templateId botMessageId stepId ->
-      { Id = ""
-        TemplateId = templateId
-        CurrentStepData =
-          { BotMessageId = botMessageId
-            ConversationStepId = stepId }
-        StepsData = [] }
-
-  // [<RequireQualifiedAccess>]
-  // module Ongoing =
-
-  type Finished =
-    { Id: ConversationId
-      TemplateId: ConversationTemplateId
-      StepsData: FinishedConversationStep list }
-
-  [<RequireQualifiedAccess>]
-  module Finished =
-    let fromNew (new': New) data : Finished =
-      { Id = new'.Id
-        StepsData = [ FinishedConversationStep.fromCurrentStepData new'.CurrentStepData data ]
-        TemplateId = new'.TemplateId }
-
-    let fromOngoing: Ongoing -> Finished =
-      fun ongoing ->
-        { Id = ongoing.Id
-          TemplateId = ongoing.TemplateId
-          StepsData = ongoing.StepsData |> List.map FinishedConversationStep.fromOngoingStep }
-
-type Conversation =
-  | New of Conversation.New
-  | Ongoing of Conversation.Ongoing
-  | Finished of Conversation.Finished
-
-type CurrentConversationId = string
-
-type CurrentConversation = { ConversationId: ConversationId }
-
-type Chat =
-  { Id: ChatId
-    CurrentConversation: CurrentConversation option }
+type IDeleteMessage =
+  abstract member DeleteMessage: chstId: ChatId * messageId: MessageId -> Task
