04660f3aaf88e7725e05fdf93b9e9bbe7d84b6c6
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-05 16:07:23 +0200

    Add shared library to load resources

---
+++ src/otsom.fs.Resources/Startup.fs
@@ -0,0 +1,17 @@
+﻿module otsom.fs.Resources.Startup
+
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Options
+open otsom.fs.Resources.Repos
+open otsom.fs.Resources.Settings
+open otsom.fs.Extensions.DependencyInjection
+
+let addResources (cfg: IConfiguration) (services: IServiceCollection) =
+  services.Configure<ResourcesSettings>(cfg.GetSection(ResourcesSettings.SectionName))
+
+  services
+    .BuildSingleton<ResourcesSettings, IOptions<ResourcesSettings>>(_.Value)
+
+    .BuildSingleton<LoadDefaultResources, ResourcesSettings, ResourceRepo.LoadLangResources>(Workflows.loadDefaultResources)
+    .BuildSingleton<LoadResources, ResourcesSettings, ResourceRepo.LoadLangResources>(Workflows.loadResources)
---
+++ src/otsom.fs.Resources/Workflows.fs
@@ -0,0 +1,50 @@
+﻿module internal otsom.fs.Resources.Workflows
+
+open otsom.fs.Resources.Repos
+open otsom.fs.Extensions.Operators
+open System
+open otsom.fs.Resources.Settings
+
+let private createResourcesMap (resources: Resource seq) =
+  resources
+  |> Seq.groupBy (_.Key)
+  |> Seq.map (fun (key, translations) -> (key, translations |> Seq.map (_.Value) |> Seq.head))
+  |> Map.ofSeq
+
+let loadDefaultResources (settings: ResourcesSettings) (loadResources: ResourceRepo.LoadLangResources) : LoadDefaultResources =
+  fun () -> task {
+    let! resourcesMap = loadResources settings.DefaultLang &|> createResourcesMap
+
+    let getResource: GetResource =
+      fun key -> resourcesMap |> Map.tryFind key |> Option.defaultValue key
+
+    let formatResource: FormatResource =
+      fun key args ->
+        resourcesMap
+        |> Map.tryFind key
+        |> Option.map (fun r -> String.Format(r, args))
+        |> Option.defaultValue key
+
+    return getResource, formatResource
+  }
+
+let loadResources (settings: ResourcesSettings) (loadResources: ResourceRepo.LoadLangResources) : LoadResources =
+  fun lang -> task {
+    let! defaultResourcesMap = loadResources settings.DefaultLang &|> createResourcesMap
+    let! resourcesMap = loadResources lang &|> createResourcesMap
+
+    let getResource: GetResource =
+      fun key ->
+        resourcesMap
+        |> Map.tryFind key
+        |> Option.defaultValue (defaultResourcesMap |> Map.tryFind key |> Option.defaultValue key)
+
+    let formatResource: FormatResource =
+      fun key args ->
+        resourcesMap
+        |> Map.tryFind key
+        |> Option.map (fun r -> String.Format(r, args))
+        |> Option.defaultValue (defaultResourcesMap |> Map.tryFind key |> Option.defaultValue key)
+
+    return getResource, formatResource
+  }
