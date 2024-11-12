2b44571fbd823631e3c741070bd34dd53a32df62
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-12 15:05:02 +0200

    Limit functions

--- src/otsom.fs.Bot/Core.fs
+++ src/otsom.fs.Bot/Core.fs
@@ -3,13 +3,12 @@ namespace otsom.fs.Bot
 open System.Threading.Tasks
 open Microsoft.FSharp.Core
 
-type ChatMessageId =
-  | BotMessageId of int
-  | UserMessageId of int
+type BotMessageId = BotMessageId of int
 
 type ChatId =
   | UserId of int64
   | GroupId of int64
   | ChannelId of int64
 
-type SendChatMessage = ChatId -> string -> Task<ChatMessageId>
+type SendMessage = string -> Task<BotMessageId>
+type SendChatMessage = ChatId -> SendMessage
