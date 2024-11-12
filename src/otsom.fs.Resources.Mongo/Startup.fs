bccf41610f894b21d9814907e6cb36c62cd5ea1e
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-12 18:18:16 +0200

    Configure Resource in MongoDB

--- src/otsom.fs.Resources.Mongo/Startup.fs
+++ src/otsom.fs.Resources.Mongo/Startup.fs
@@ -2,12 +2,18 @@ module otsom.fs.Resources.Mongo.Startup
 
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
+open MongoDB.Bson.Serialization
 open MongoDB.Driver
 open otsom.fs.Extensions.DependencyInjection
 open otsom.fs.Resources.Mongo.Repos
 open otsom.fs.Resources.Repos
 
 let addMongoResources (cfg: IConfiguration) (services: IServiceCollection) =
+
+  BsonClassMap.RegisterClassMap<Resource>(fun cm ->
+    cm.AutoMap()
+    cm.SetIgnoreExtraElements true
+    ())
 
   services
     .BuildSingleton<IMongoCollection<Resource>, IMongoDatabase>(_.GetCollection("resources"))
