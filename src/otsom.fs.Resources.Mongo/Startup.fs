88d89998ff333c01b0e853cd528a600f6d7df642
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-11-12 14:51:22 +0200

    Remove usings

--- src/otsom.fs.Resources.Mongo/Startup.fs
+++ src/otsom.fs.Resources.Mongo/Startup.fs
@@ -2,14 +2,7 @@ module otsom.fs.Resources.Mongo.Startup
 
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Options
-open MongoDB.Bson
-open MongoDB.Bson.Serialization
-open MongoDB.Bson.Serialization.Attributes
-open MongoDB.Bson.Serialization.IdGenerators
-open MongoDB.Bson.Serialization.Serializers
 open MongoDB.Driver
-open otsom.fs.Resources
 open otsom.fs.Extensions.DependencyInjection
 open otsom.fs.Resources.Mongo.Repos
 open otsom.fs.Resources.Repos
