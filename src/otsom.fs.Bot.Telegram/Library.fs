514dbb07366e0e321285ddb0773ae719f3bd72f9
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-12-08 19:42:03 +0200

    Add SendChatMessageButtons implementation to Bot.Telegram

--- src/otsom.fs.Bot.Telegram/Library.fs
+++ src/otsom.fs.Bot.Telegram/Library.fs
@@ -1,5 +1,6 @@
 namespace otsom.fs.Bot.Telegram
 
+open Telegram.Bot.Types.Enums
 open otsom.fs.Bot
 open Telegram.Bot
 open Telegram.Bot.Types
@@ -11,7 +12,7 @@ module internal Workflows =
 
   let sendChatMessage (bot: ITelegramBotClient) : SendChatMessage =
     fun chatId text ->
-      bot.SendTextMessageAsync((chatId.Value |> ChatId), text |> escapeMarkdownString)
+      bot.SendTextMessageAsync((chatId.Value |> ChatId), text |> escapeMarkdownString, parseMode = ParseMode.MarkdownV2)
       &|> (_.MessageId >> BotMessageId)
 
   let private getReplyMarkup (keyboard: Keyboard) =
@@ -19,7 +20,12 @@ module internal Workflows =
 
   let sendChatKeyboard (bot: ITelegramBotClient) : SendChatKeyboard =
     fun chatId text keyboard ->
-      bot.SendTextMessageAsync((chatId.Value |> ChatId), text |> escapeMarkdownString, replyMarkup = getReplyMarkup keyboard)
+      bot.SendTextMessageAsync(
+        (chatId.Value |> ChatId),
+        text |> escapeMarkdownString,
+        replyMarkup = getReplyMarkup keyboard,
+        parseMode = ParseMode.MarkdownV2
+      )
       &|> (_.MessageId >> BotMessageId)
 
   let deleteBotMessage (bot: ITelegramBotClient) : DeleteBotMessage =
@@ -27,7 +33,7 @@ module internal Workflows =
 
   let editBotMessage (bot: ITelegramBotClient) : EditBotMessage =
     fun chatId messageId text ->
-      bot.EditMessageTextAsync((chatId.Value |> ChatId), messageId.Value, text |> escapeMarkdownString)
+      bot.EditMessageTextAsync((chatId.Value |> ChatId), messageId.Value, text |> escapeMarkdownString, parseMode = ParseMode.MarkdownV2)
       &|> ignore
 
   let private getInlineMarkup (buttons: MessageButtons) =
@@ -35,12 +41,23 @@ module internal Workflows =
     |> Seq.map (Seq.map InlineKeyboardButton.WithCallbackData)
     |> InlineKeyboardMarkup
 
+  let sendChatMessageButtons (bot: ITelegramBotClient) : SendChatMessageButtons =
+    fun chatId text buttons ->
+      bot.SendTextMessageAsync(
+        (chatId.Value |> ChatId),
+        text |> escapeMarkdownString,
+        replyMarkup = getInlineMarkup buttons,
+        parseMode = ParseMode.MarkdownV2
+      )
+      &|> (_.MessageId >> BotMessageId)
+
   let editBotMessageButtons (bot: ITelegramBotClient) : EditBotMessageButtons =
     fun chatId messageId text buttons ->
       bot.EditMessageTextAsync(
         (chatId.Value |> ChatId),
         messageId.Value,
         text |> escapeMarkdownString,
-        replyMarkup = getInlineMarkup buttons
+        replyMarkup = getInlineMarkup buttons,
+        parseMode = ParseMode.MarkdownV2
       )
       &|> ignore
