7a4f7aa29f96a34c4e7a188d51c2a9f948112c4d
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-12-08 14:46:47 +0200

    Add Delete Message

--- src/otsom.fs.Bot.Telegram/Startup.fs
+++ src/otsom.fs.Bot.Telegram/Startup.fs
@@ -5,7 +5,17 @@ open Microsoft.Extensions.DependencyInjection
 open Telegram.Bot
 open otsom.fs.Bot
 open otsom.fs.Extensions.DependencyInjection
+open otsom.fs.Bot.Telegram.Workflows
+
+let internal buildChatContext (bot: ITelegramBotClient) : BuildChatContext =
+  fun chatId ->
+    { new IChatContext with
+        member this.SendMessage = sendChatMessage bot chatId
+        member this.DeleteBotMessage = deleteBotMessage bot }
 
 let addTelegramBot (cfg: IConfiguration) (services: IServiceCollection) =
   services
-    .BuildSingleton<DeleteBotMessage, ITelegramBotClient>(Workflows.deleteBotMessage)
+    .BuildSingleton<DeleteBotMessage, ITelegramBotClient>(deleteBotMessage)
+    .BuildSingleton<SendChatMessage, ITelegramBotClient>(sendChatMessage)
+
+  services.BuildSingleton<BuildChatContext, ITelegramBotClient>(buildChatContext)
