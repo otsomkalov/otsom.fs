d45b22dc5053985d4d9f0e69244e1af85091b0c4
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2024-12-24 19:37:23 +0200

    Move Auth Entities into Auth.Mongo project

--- src/otsom.fs.Telegram.Bot.Auth.Spotify.Mongo/Mappings.fs
+++ src/otsom.fs.Telegram.Bot.Auth.Spotify.Mongo/Mappings.fs
@@ -1,6 +1,6 @@
 namespace otsom.fs.Telegram.Bot.Auth.Spotify.Mongo
 
-open otsom.fs.Core
+open otsom.fs.Telegram.Bot.Auth.Spotify
 open otsom.fs.Telegram.Bot.Auth.Spotify.Mongo
 open otsom.fs.Telegram.Bot.Auth.Spotify.Workflows
 
@@ -9,26 +9,26 @@ module internal Mappings =
   module Inited =
     let fromDb (auth: Entities.Auth) : Inited =
       { State = State.parse auth.State
-        UserId = auth.UserId |> UserId }
+        AccountId = auth.AccountId |> AccountId }
 
     let toDb (auth: Inited) : Entities.Auth =
-      Entities.Auth(UserId = (auth.UserId |> UserId.value), State = (auth.State |> State.value))
+      Entities.Auth(AccountId = auth.AccountId.Value, State = auth.State.Value)
 
   [<RequireQualifiedAccess>]
   module Fulfilled =
     let fromDb (auth: Entities.Auth) : Fulfilled =
       { State = State.parse auth.State
-        UserId = auth.UserId |> UserId
+        AccountId = auth.AccountId |> AccountId
         Code = Code auth.Code }
 
     let toDb (auth: Fulfilled) : Entities.Auth =
-      Entities.Auth(UserId = (auth.UserId |> UserId.value), State = (auth.State |> State.value), Code = (auth.Code |> Code.value))
+      Entities.Auth(AccountId = auth.AccountId.Value, State = auth.State.Value, Code = (auth.Code |> Code.value))
 
   [<RequireQualifiedAccess>]
   module Completed =
     let toDb (auth: Completed) : Entities.UserToken =
-      Entities.UserToken(UserId = (auth.UserId |> UserId.value), RefreshToken = auth.Token)
+      Entities.UserToken(AccountId = auth.AccountId.Value, RefreshToken = auth.Token)
 
     let fromDb (token: Entities.UserToken) : Completed =
-      { UserId = (token.UserId |> UserId)
+      { AccountId = (token.AccountId |> AccountId)
         Token = token.RefreshToken }
