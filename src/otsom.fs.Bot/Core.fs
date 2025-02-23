21efa12f70462fe614a260aeb2cb5ef3506b947e
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-02-23 13:35:05 +0200

    Update Bot API

--- src/otsom.fs.Bot/Core.fs
+++ src/otsom.fs.Bot/Core.fs
@@ -20,38 +20,38 @@ type ChatId =
   member this.Value = let (ChatId id) = this in id
 
 type ISendMessage =
-  abstract SendMessage: ChatId * string -> Task<BotMessageId>
+  abstract SendMessage: string -> Task<BotMessageId>
 
 type IDeleteBotMessage =
-  abstract DeleteBotMessage: ChatId * BotMessageId -> Task
+  abstract DeleteBotMessage: BotMessageId -> Task
 
 type KeyboardButton = string
 
 type Keyboard = KeyboardButton seq seq
 
 type ISendKeyboard =
-  abstract SendKeyboard: ChatId * string * Keyboard -> Task<BotMessageId>
+  abstract SendKeyboard: string * Keyboard -> Task<BotMessageId>
 
 type IEditMessage =
-  abstract EditMessage: ChatId * BotMessageId * string -> Task<unit>
+  abstract EditMessage: BotMessageId * string -> Task<unit>
 
 type MessageButton = string * string
 type MessageButtons = MessageButton seq seq
 
 type ISendMessageButtons =
-  abstract SendMessageButtons: ChatId * string * MessageButtons -> Task<BotMessageId>
+  abstract SendMessageButtons: string * MessageButtons -> Task<BotMessageId>
 
 type IAskForReply =
-  abstract AskForReply: ChatId * string -> Task<unit>
+  abstract AskForReply: string -> Task<unit>
 
 type IEditMessageButtons =
-  abstract EditMessageButtons: ChatId * BotMessageId * string * MessageButtons -> Task<unit>
+  abstract EditMessageButtons: BotMessageId * string * MessageButtons -> Task<unit>
 
 type IReplyToMessage =
-  abstract ReplyToMessage: ChatId * ChatMessageId * string -> Task<BotMessageId>
+  abstract ReplyToMessage: ChatMessageId * string -> Task<BotMessageId>
 
 type ISendLink =
-  abstract SendLink: ChatId * string * string * Uri -> Task<BotMessageId>
+  abstract SendLink: string * string * Uri -> Task<BotMessageId>
 
 type ButtonClickId =
   | ButtonClickId of string
@@ -59,7 +59,7 @@ type ButtonClickId =
   member this.Value = let (ButtonClickId id) = this in id
 
 type ISendNotification =
-  abstract SendNotification: ChatId * ButtonClickId * string -> Task<unit>
+  abstract SendNotification: ButtonClickId * string -> Task<unit>
 
 type IBotService =
   inherit ISendMessage
@@ -77,3 +77,5 @@ type IBotService =
   inherit IAskForReply
 
   inherit ISendNotification
+
+type BuildBotService = ChatId -> IBotService
