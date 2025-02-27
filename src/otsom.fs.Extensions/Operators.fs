1a8d232f5398cb5196d27981d47b01e2baa31222
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-02-27 13:27:34 +0200

    Hide operators from stack trace

--- src/otsom.fs.Extensions/Operators.fs
+++ src/otsom.fs.Extensions/Operators.fs
@@ -1,29 +1,40 @@
 ﻿[<AutoOpen>]
 module otsom.fs.Extensions.Operators
 
+open System.Diagnostics
+
 /// Task.map
+[<StackTraceHidden>]
 let (&|>) arg func = Task.map func arg
 
 /// Task.tap
+[<StackTraceHidden>]
 let (&|!) arg func = Task.tap func arg
 
 /// Task.bind
+[<StackTraceHidden>]
 let (&|&>) arg func = Task.bind func arg
 
 /// Result.map
+[<StackTraceHidden>]
 let (=|>) arg func = Result.map func arg
 
 /// Result.bind
+[<StackTraceHidden>]
 let (=|=>) arg func = Result.bind func arg
 
 /// Result.taskMap
+[<StackTraceHidden>]
 let (=|&>) arg func = Result.taskMap func arg
 
 /// TaskResult.map
+[<StackTraceHidden>]
 let (&=|>) arg func = TaskResult.map func arg
 
 /// TaskResult.taskMap
+[<StackTraceHidden>]
 let (&=|&>) arg func = TaskResult.taskMap func arg
 
 /// TaskResult.taskTap
+[<StackTraceHidden>]
 let (&=|&!) arg func = TaskResult.taskTap func arg
