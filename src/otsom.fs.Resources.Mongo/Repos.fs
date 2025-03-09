ce28e7a4a6baa705ac42f2cba2167ae4706cf579
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2025-03-09 20:53:35 +0200

    Update MongoDB lib to version 3

--- src/otsom.fs.Resources.Mongo/Repos.fs
+++ src/otsom.fs.Resources.Mongo/Repos.fs
@@ -2,10 +2,11 @@ namespace otsom.fs.Resources.Mongo
 
 open MongoDB.Driver
 open otsom.fs.Resources
-open MongoDB.Driver.Linq
 open otsom.fs.Extensions.Operators
 
 type ResourceRepo(collection: IMongoCollection<Resource>) =
   interface IResourceRepo with
     member this.LoadResources(lang) =
-      collection.AsQueryable().Where(fun r -> r.Lang = lang).ToListAsync() &|> seq
+      let filter = Builders<Resource>.Filter.Eq(_.Lang, lang)
+
+      collection.Find(filter).ToListAsync() &|> seq
