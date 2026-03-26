bb66b4ad48fa2c2ebf64d47a6f444b4bc33a4f4b
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2026-03-26 19:48:16 +0200

    Format tests

--- tests/otsom.fs.Bot.Tests/Builders.fs
+++ tests/otsom.fs.Bot.Tests/Builders.fs
@@ -6,114 +6,118 @@ open otsom.fs.Bot
 open otsom.fs.Bot.Builders
 
 type MockChat() =
-    interface IChat with
-        member _.Id = ChatId 1L
+  interface IChat with
+    member _.Id = ChatId 1L
 
 type ClickHandlersBuilderTests() =
 
-    let chat = MockChat() :> IChat
-    let click = { Id = ButtonClickId "1"; MessageId = BotMessageId 1; Data = [] }
+  let chat = MockChat() :> IChat
 
-    [<Fact>]
-    member _.``Return and Zero return None``() = task {
-        let h1 = clickHandlers { return () }
-        let h2 = clickHandlers { () }
+  let click =
+    { Id = ButtonClickId "1"
+      MessageId = BotMessageId 1
+      Data = [] }
 
-        let! r1 = h1 chat click
-        let! r2 = h2 chat click
+  [<Fact>]
+  member _.``Return and Zero return None``() = task {
+    let h1 = clickHandlers { return () }
+    let h2 = clickHandlers { () }
 
-        Assert.Equal(None, r1)
-        Assert.Equal(None, r2)
+    let! r1 = h1 chat click
+    let! r2 = h2 chat click
+
+    Assert.Equal(None, r1)
+    Assert.Equal(None, r2)
+  }
+
+  [<Fact>]
+  member _.``and YieldFrom return the handler``() = task {
+    let expected = Some()
+    let handler: ClickHandler<IChat> = fun _ _ -> Task.FromResult(expected)
+
+    let h1 = clickHandlers { handler }
+    let h2 = clickHandlers { handler }
+
+    let! r1 = h1 chat click
+    let! r2 = h2 chat click
+
+    Assert.Equal(expected, r1)
+    Assert.Equal(expected, r2)
+  }
+
+  [<Fact>]
+  member _.``Combine returns Some if first is Some``() = task {
+    let h1: ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some())
+    let h2: ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+
+    let h = clickHandlers {
+      h1
+      h2
     }
 
-    [<Fact>]
-    member _.``Yield and YieldFrom return the handler``() = task {
-        let expected = Some ()
-        let handler : ClickHandler<IChat> = fun _ _ -> Task.FromResult(expected)
+    let! r = h chat click
 
-        let h1 = clickHandlers { yield handler }
-        let h2 = clickHandlers { yield! handler }
+    Assert.Equal(Some(), r)
+  }
 
-        let! r1 = h1 chat click
-        let! r2 = h2 chat click
+  [<Fact>]
+  member _.``Combine returns Some if second is Some``() = task {
+    let h1: ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+    let h2: ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some())
 
-        Assert.Equal(expected, r1)
-        Assert.Equal(expected, r2)
+    let h = clickHandlers {
+      h1
+      h2
     }
 
-    [<Fact>]
-    member _.``Combine returns Some if first is Some``() = task {
-        let h1 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some ())
-        let h2 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+    let! r = h chat click
 
-        let h = clickHandlers {
-            yield h1
-            yield h2
-        }
+    Assert.Equal(Some(), r)
+  }
 
-        let! r = h chat click
+  [<Fact>]
+  member _.``Combine returns Some if both are Some``() = task {
+    let h1: ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some())
+    let h2: ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some())
 
-        Assert.Equal(Some (), r)
+    let h = clickHandlers {
+      h1
+      h2
     }
 
-    [<Fact>]
-    member _.``Combine returns Some if second is Some``() = task {
-        let h1 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
-        let h2 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some ())
+    let! r = h chat click
 
-        let h = clickHandlers {
-            yield h1
-            yield h2
-        }
+    Assert.Equal(Some(), r)
+  }
 
-        let! r = h chat click
+  [<Fact>]
+  member _.``Combine returns None if both are None``() = task {
+    let h1: ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+    let h2: ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
 
-        Assert.Equal(Some (), r)
+    let h = clickHandlers {
+      h1
+      h2
     }
 
-    [<Fact>]
-    member _.``Combine returns Some if both are Some``() = task {
-        let h1 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some ())
-        let h2 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some ())
+    let! r = h chat click
 
-        let h = clickHandlers {
-            yield h1
-            yield h2
-        }
+    Assert.Equal(None, r)
+  }
 
-        let! r = h chat click
+  [<Fact>]
+  member _.``Multiple yields combine correctly``() = task {
+    let h1: ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+    let h2: ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+    let h3: ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some())
 
-        Assert.Equal(Some (), r)
+    let h = clickHandlers {
+      h1
+      h2
+      h3
     }
 
-    [<Fact>]
-    member _.``Combine returns None if both are None``() = task {
-        let h1 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
-        let h2 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
+    let! r = h chat click
 
-        let h = clickHandlers {
-            yield h1
-            yield h2
-        }
-
-        let! r = h chat click
-
-        Assert.Equal(None, r)
-    }
-
-    [<Fact>]
-    member _.``Multiple yields combine correctly``() = task {
-        let h1 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
-        let h2 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(None)
-        let h3 : ClickHandler<IChat> = fun _ _ -> Task.FromResult(Some ())
-
-        let h = clickHandlers {
-            yield h1
-            yield h2
-            yield h3
-        }
-
-        let! r = h chat click
-
-        Assert.Equal(Some (), r)
-    }
+    Assert.Equal(Some(), r)
+  }
