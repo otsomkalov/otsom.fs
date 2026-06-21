d68fe95d6baf96817aa8afab967694518dd90ad9
Oleg Tsomkalov <16070015+otsomkalov@users.noreply.github.com>
2026-06-21 14:49:42 +0300

    Add extensions for encryption (#219)

---
+++ src/otsom.fs.Extensions/Crypto.fs
@@ -0,0 +1,51 @@
+﻿module otsom.fs.Extensions.Crypto
+
+open System
+open System.Security.Cryptography
+open System.Text
+
+[<Literal>]
+let private SaltSize = 16
+
+[<Literal>]
+let private NonceSize = 12
+
+[<Literal>]
+let private TagSize = 16
+
+[<Literal>]
+let KeyDeriveIterations = 100_000
+
+let encrypt (key: string) (plaintext: string) =
+  let salt = RandomNumberGenerator.GetBytes SaltSize
+  let nonce = RandomNumberGenerator.GetBytes NonceSize // Recommended size for GCM
+
+  let key =
+    Rfc2898DeriveBytes.Pbkdf2(key, salt, KeyDeriveIterations, HashAlgorithmName.SHA256, 32)
+
+  let plaintextBytes = Encoding.UTF8.GetBytes plaintext
+  let ciphertext = Array.zeroCreate<byte> plaintextBytes.Length
+  let tag = Array.zeroCreate<byte> TagSize
+
+  use aes = new AesGcm(key, TagSize)
+  aes.Encrypt(nonce, plaintextBytes, ciphertext, tag)
+
+  Array.concat [ salt; nonce; tag; ciphertext ] |> Convert.ToBase64String
+
+let decrypt (key: string) (encrypted: string) =
+  let data = Convert.FromBase64String encrypted
+
+  let salt = data.[0..15]
+  let nonce = data.[16..27]
+  let tag = data.[28..43]
+  let ciphertext = data.[44..]
+
+  let key =
+    Rfc2898DeriveBytes.Pbkdf2(key, salt, KeyDeriveIterations, HashAlgorithmName.SHA256, 32)
+
+  let plaintext = Array.zeroCreate<byte> ciphertext.Length
+
+  use aes = new AesGcm(key, TagSize)
+  aes.Decrypt(nonce, ciphertext, tag, plaintext)
+
+  Encoding.UTF8.GetString plaintext
---
+++ tests/otsom.fs.Extensions.Tests/Crypto.fs
@@ -0,0 +1,35 @@
+﻿module Crypto
+
+open Xunit
+open FsUnit.Xunit
+open otsom.fs.Extensions.Crypto
+
+[<Fact>]
+let ``Should encrypt and decrypt string`` () =
+  let key = "test-key"
+  let plainText = "hello world"
+
+  let encrypted = encrypt key plainText
+  let decrypted = decrypt key encrypted
+
+  decrypted |> should equal plainText
+
+[<Fact>]
+let ``Different IVs for same text and key`` () =
+  let key = "test-key"
+  let plainText = "hello world"
+
+  let encrypted1 = encrypt key plainText
+  let encrypted2 = encrypt key plainText
+
+  encrypted1 |> should not' (equal encrypted2)
+
+[<Fact>]
+let ``Should fail to decrypt with wrong key`` () =
+  let key1 = "key1"
+  let key2 = "key2"
+  let plainText = "hello world"
+  let encrypted = encrypt key1 plainText
+
+  (fun () -> decrypt key2 encrypted |> ignore)
+  |> should throw typeof<System.Security.Cryptography.CryptographicException>
